60b03dbe63de9df32248c7fb2920012f48b00388
Manisha Sanginkar <sanginkarmanisha2021@gmail.com>
2023-07-27 21:04:38 +0530

    first commit

--- server.js
+++ server.js
@@ -2,20 +2,20 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/UserRoutes");
 const mongoose = require("mongoose");
-
+const dotenv = require ("dotenv");
 const app = express();
 app.use(cors());                   
 app.use(express.json());
-
+dotenv.config();
 mongoose
-  .connect("mongodb+srv://sanginkarmanisha2021:edtmP52BefJjklyf@cluster0.qocnual.mongodb.net/netflix2", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  // .connect(process.env.URL, {
+  // .connect("mongodb+srv://sanginkarmanisha2021:edtmP52BefJjklyf@cluster0.qocnual.mongodb.net/netflix2", {
   //   useNewUrlParser: true,
   //   useUnifiedTopology: true,
   // })
+  .connect(process.env.URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log("DB Connetion Successfull");
   });
@@ -25,7 +25,7 @@ mongoose
 
 app.use("/api/user", userRoutes);
 
-app.listen(5000, console.log("server started "));
+app.listen(4000, console.log("server started "));
 
 
 
